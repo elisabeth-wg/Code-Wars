65dd28cf90539d53fae0c36b81abcc611c18d5d4
Eliasbeth Gliddon <elisabeth.gliddon@gmail.com>
2022-12-14 18:12:46 -0600

    Coding challenge 12-14-2022

---
+++ CC2022-12-14/main.js
@@ -0,0 +1,23 @@
+// Will you make it ?
+    
+// DESCRIPTION:
+// You were camping with your friends far away from home, but when it's time to go back, you realize that your fuel is running out and the nearest pump is 50 miles away! You know that on average, your car runs on about 25 miles per gallon. There are 2 gallons left.
+
+// Considering these factors, write a function that tells you if it is possible to get to the pump or not.
+
+// Function should return true if it is possible and false if not.
+
+
+const zeroFuel = (distance, mpg, gal) => {
+    return ((gal*mpg) >= distance) ? true : false;
+}
+
+//or
+
+function zeroFuel(distance, mpg, gal) {
+    if ((gal * mpg) >= distance) {
+       return true
+    } else {
+        return false
+   }
+}
