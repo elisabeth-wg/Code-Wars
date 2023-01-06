66eab6c33564fc97736c6048fbb400946ec14c52
Eliasbeth Gliddon <elisabeth.gliddon@gmail.com>
2023-01-06 09:09:40 -0600

    Coding challenge 1-6-2023

---
+++ CC2023-01-06/main.js
@@ -0,0 +1,9 @@
+// MakeUpperCase
+
+
+// DESCRIPTION:
+// Write a function which converts the input string to uppercase.
+
+
+
+const makeUpperCase = (str) => str.length > 0 ? str.toUpperCase() : str
