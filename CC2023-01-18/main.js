c520e2e5a3cc51e1296cac449f1c9b2c5b353808
Eliasbeth Gliddon <elisabeth.gliddon@gmail.com>
2023-01-18 10:04:25 -0600

    Coding challenge 1-18-2023

---
+++ CC2023-01-18/main.js
@@ -0,0 +1,19 @@
+// If you can't sleep, just count sheep!!
+
+// DESCRIPTION:
+// If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+const countSheep = (num) => {
+  let murmurArr = []
+    if(num > 0){
+      for (let i = 1; i <= num; i++) {
+        murmurArr.push(`${i} sheep...`)
+      }
+    }else{
+      return ''
+    }
+  return murmurArr.join('')
+}
