497e81dc2d7f250f9fea6c3454d95205b81d3414
Eliasbeth Gliddon <elisabeth.gliddon@gmail.com>
2023-03-03 12:59:47 -0600

    Coding challenge 3-3-23 - Students Final Grade

---
+++ CC2023-03-03/main.js
@@ -0,0 +1,54 @@
+
+// Student's Final Grade
+
+// Create a function finalGrade, which calculates the final grade of a student depending on two parameters: a grade for the exam and a number of completed projects.
+
+// This function should take two arguments: exam - grade for exam (from 0 to 100); projects - number of completed projects (from 0 and above);
+
+// This function should return a number (final grade). There are four types of final grades:
+
+// 100, if a grade for the exam is more than 90 or if a number of completed projects more than 10.
+// 90, if a grade for the exam is more than 75 and if a number of completed projects is minimum 5.
+// 75, if a grade for the exam is more than 50 and if a number of completed projects is minimum 2.
+// 0, in other cases
+// Examples(Inputs-->Output):
+
+// 100, 12 --> 100
+// 99, 0 --> 100
+// 10, 15 --> 100
+
+// 85, 5 --> 90
+
+// 55, 3 --> 75
+
+// 55, 0 --> 0
+// 20, 2 --> 0
+// *Use Comparison and Logical Operators.
+
+
+// function finalGrade (exam, projects) {
+//   return // final grade
+// }
+
+
+//P: Takes in two numbers, one is 0-100 and another 0+ - positive integers only
+
+//R: A final grade number 0, 75, 90, 100 
+
+//E: ( 100, 12 --> 100), (99, 0 --> 100), (20, 2 --> 0) 
+
+//P: Take in two integer paramenters, set up if/else statements with ||. Return correct
+//grade as noted in Return
+
+
+function finalGrade (exam, projects) {
+  if(exam > 90 || projects > 10){
+    return 100
+  }else if(exam > 75 && projects >= 5){
+    return 90
+  }else if (exam > 50 && projects >= 2){
+    return 75
+  }else {
+    return 0
+  }
+}
