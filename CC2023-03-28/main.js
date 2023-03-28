289988dcb38da95ff65377497c872258af2754a3
Eliasbeth Gliddon <elisabeth.gliddon@gmail.com>
2023-03-28 10:45:21 -0500

    Coding challenge 3-28-2023 - Type of Sum

---
+++ CC2023-03-28/main.js
@@ -0,0 +1,19 @@
+
+// Type of sum
+
+// Return the type of the sum of the two arguments
+
+// function typeOfSum(a, b) {
+//   // good luck
+// }
+
+
+//P: Takes in two parameters
+
+//R: returns the type of the sum
+
+//E: ('d', 1), 'string'), (true, 1), 'number'); (undefined, 're'), 'string');
+
+//P: take in two parameters, return type of their sum.
+
+const typeOfSum = (a, b) => typeof(a + b);
