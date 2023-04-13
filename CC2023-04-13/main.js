013547af1d5a50b5ac5867921a2a02925ebc8ce8
Eliasbeth Gliddon <elisabeth.gliddon@gmail.com>
2023-04-13 11:55:54 -0500

    Coding challenge 4-13-23 - Chuck Norris VII

---
+++ CC2023-04-13/main.js
@@ -0,0 +1,24 @@
+// Chuck Norris VII - True or False ? 
+
+// It's a well known fact that anything Chuck Norris wants, he gets. As a result Chuck very rarely has to use the word false.
+
+// It is a less well known fact that if something is true, and Chuck doesn't want it to be, Chuck can scare the truth with his massive biceps, and it automatically becomes false.
+
+// Your task is to be more like Chuck (ha! good luck!). You must return false without ever actually using the word false...
+
+// Go show some truth who's boss!
+
+function ifChuckSaysSo(){
+
+}
+
+
+//P: Does not specify what parameters it takes in, seemingly could be anything.     
+
+//R: return false without ever using the word false
+
+//E: (3, false), ('cat', false), ([fruit, vegetables], false)
+
+//P: Take in argument, create a conditional asking if it equals its opposite with a bang. Should return false. 
+
+const ifChuckSaysSo = (x) => x === !x
