02a47ab6be277ba3b53b1a1fb8b6e8e7f383d009
Eliasbeth Gliddon <elisabeth.gliddon@gmail.com>
2023-04-17 09:47:52 -0500

    Coding challenge 4-17-23 - 5 without numbers

---
+++ CC2023-04-17/main.js
@@ -0,0 +1,22 @@
+// 5 without numbers!!
+
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck:)
+
+// function unusualFive() {
+//   return 5;
+// }
+
+
+//P: Can take in any parameter - string, boolean, array, number
+
+//R: Always returns 5
+
+//E: ('orange' --> 5), (456789 --> 5), (true --> 5)
+
+//P: Take in any parameter. Find the length of a string which is five characters long and return the length. 
+
+const unusualFive = (x) => 'hello'.length
