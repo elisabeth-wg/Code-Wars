b9868b1356ecd064dea327ee5725f5f5a3edf54d
Eliasbeth Gliddon <elisabeth.gliddon@gmail.com>
2023-05-10 21:47:45 -0500

    Coding challenge 5-10-23 - Switch Case - Bug fixing Number 6

--- CC2023-05-10/main.js
+++ CC2023-05-10/main.js
@@ -24,21 +24,22 @@
 
 //P: I will check that the switch case as the correct breaks. I will make sure that the syntax is correct. 
 
-function evalObject(value){
-  var result = 0;
-  switch(value.operation){
-    case'+': result = value.a + value.b;
-    break;
-    case'-': result = value.a - value.b;
-    break;
-    case'/': result = value.a / value.b;
-    break;
-    case'*': result = value.a * value.b;
-    break;
-    case'%': result = value.a % value.b;
-    break;
-    case'^': result = Math.pow(value.a, value.b);
-    break;
-  }
+function evalObject(value) {
+    var result = 0;
+    switch (value.operation) {
+        case '+': result = value.a + value.b;
+            break;
+        case '-': result = value.a - value.b;
+            break;
+        case '/': result = value.a / value.b;
+            break;
+        case '*': result = value.a * value.b;
+            break;
+        case '%': result = value.a % value.b;
+            break;
+        case '^': result = Math.pow(value.a, value.b);
+            break;
+    }
+}
     
     //Needed breaks
