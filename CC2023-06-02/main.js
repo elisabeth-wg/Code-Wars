8204daf71d03b6f08d6bf9d123509a3ebe1bb2fb
Eliasbeth Gliddon <elisabeth.gliddon@gmail.com>
2023-06-02 09:12:18 -0500

    Coding challenge 6-2-23 - Nota == a

---
+++ CC2023-06-02/main.js
@@ -0,0 +1,21 @@
+// !a == a ? !
+
+// DESCRIPTION:
+// You task to pass only this tests :
+
+// a == false
+// !a == false
+// a == !a
+
+//P: Takes in 'a' variable
+
+
+//R: Doesn't return anything. 
+
+
+//E: It only needs to meet the qualifications, something like a zero. 
+
+
+//P: Need something that when it is isn't opposite it is still the same. But also is false on its own. empty brackets could be the solution.
+
+const a = []
