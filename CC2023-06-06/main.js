cf9baf9546adf5ca55a4a0d2874c254ad4390a3c
Eliasbeth Gliddon <elisabeth.gliddon@gmail.com>
2023-06-06 18:41:26 -0500

    Coding challenge - 6-6-23 - Ones and Zeros

---
+++ CC2023-06-06/main.js
@@ -0,0 +1,37 @@
+// Ones and Zeros
+
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// Examples:
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+// However, the arrays can have varying lengths, not just limited to 4.
+
+// const binaryArrayToNumber = arr => {
+//   // your code
+// };
+
+//P: Takes in an array of ones and zeros only.
+
+//R: Return the binary equivilant to those array items as if they were a string of numbers vs array elements.
+
+//E: ([0, 0, 1, 0] --> 2)
+//   ([1, 0, 1, 1] --> 11)
+//   ([1, 1, 1, 1] --> 15)
+
+//P: Take in the array of numbers and join into a string. Use the parstInt() method to convert them to binary with the 2 in the base number and return.
+
+const binaryArrayToNumber = arr => {
+  let binary = arr.join('')
+  let digit = parseInt(binary, 2)
+  return digit
+};
