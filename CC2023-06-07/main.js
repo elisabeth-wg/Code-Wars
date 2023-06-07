6e478bb65e523ad0e625759335e13e99f4e42f9a
Eliasbeth Gliddon <elisabeth.gliddon@gmail.com>
2023-06-07 07:00:50 -0500

    Coding challenege 6-7-23 - Convert a number to a string

---
+++ CC2023-06-07/main.js
@@ -0,0 +1,27 @@
+// Convert a Number to a String!
+
+// DESCRIPTION:
+// We need a function that can transform a number (integer) into a string.
+
+// What ways of achieving this do you know?
+
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+
+// function numberToString(num) {
+//   // Return a string of the number here!
+// }
+
+//P: Takes in a positive or negative number.
+
+//R: Returns that number as a string. 
+
+//E: (312 --> "312")
+//   (-234 --> "-234")
+//   (8765 --> "8765")
+
+//P: Take in number and use string method .toString() to convert to string and return.
+
+const numberToString = (num) => num.toString()
