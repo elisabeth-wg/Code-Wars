129163d99f9d82d48231234e8d5f73b183bdc2ab
Elisabeth Gliddon <elisabeth.gliddon@gmail.com>
2023-07-21 16:04:46 -0500

    Coding challenge 7-21-23 - Reverse List Order

---
+++ CC2023-07-21/main.js
@@ -0,0 +1,23 @@
+// Reverse List Order
+
+//  In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// Examples (Input -> Output)
+// * [1, 2, 3, 4]  -> [4, 3, 2, 1]
+//     * [9, 2, 0, 7] -> [7, 0, 2, 9]
+
+// function reverseList(list) {
+
+// }
+
+//PREP:
+//Take in an array of numbers. All positive.
+//Return the array in reverse.
+//[33, 10, 5, 4] -> [4, 5, 10, 33]
+//[5, 7, 2] -> [2, 7, 5]
+//[100, 2, 1] -> [1, 2, 100]
+//Take in the array. Use the reverse method and return.
+
+
+
+const reverseList = (list) => list.reverse()
