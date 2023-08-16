4fe7a56e0f7ab13595ce0058133ce031648e67de
Elisabeth Gliddon <elisabeth.gliddon@gmail.com>
2023-08-16 14:36:20 -0500

    Coding challenge 8-16-23 - Kata Example Twist

---
+++ CC2023-08-16/main.js
@@ -0,0 +1,17 @@
+// Kata Example Twist
+
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the array websites 1,000 times.
+
+// // add the value "codewars" to the websites array 1,000 times
+// var websites = []
+
+//PREP
+//Given a website array which is empty. And string of "codewars"
+//The website array should have 1000 "codewars" strings for elements
+//console.log(websites.length > 0, 'The array is still empty')
+//Create a loop adding the string 1000x to the array. Or use the .fill method to fill the array with 'codewars'
+
+
+let websites = new Array(1000).fill('codewars')
