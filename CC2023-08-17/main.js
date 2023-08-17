d8cfe94e505236f4c16c144b8f91bcffd089f456
Elisabeth Gliddon <elisabeth.gliddon@gmail.com>
2023-08-17 16:51:13 -0500

    Coding challenge 8-17-23 - Grasshopper Function syntax debugging

---
+++ CC2023-08-17/main.js
@@ -0,0 +1,18 @@
+// Grasshopper - Function syntax debugging
+
+// A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+
+// function main [verb, noun]
+//   return verb + noun
+// }
+
+//PREP
+//We are given a function which has syntactical mistakes.
+//We are to fix the function so that it works properly
+//It should add the two parameters together and return their value
+//Examine the function and fix any errors.
+//Needed to change the square brackets to parentheses. Needed to add an opening curly brace.
+
+function main (verb, noun){
+  return verb + noun
+}
