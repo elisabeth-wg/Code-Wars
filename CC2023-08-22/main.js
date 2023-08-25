d313592b818a69b52242aef68173e92e7c720afa
Elisabeth Gliddon <elisabeth.gliddon@gmail.com>
2023-08-25 11:39:55 -0500

    Coding challenge 8-22-23 - List Filtering

---
+++ CC2023-08-22/main.js
@@ -0,0 +1,21 @@
+// List Filtering
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+}
+
+
+//PREP:
+// Given a list of non-negative integers (could be zero) and strings.
+// Return a list without the strings.
+// console.log(filter_list([1,2,'a','b']),[1,2]);
+// console.log(filter_list([0,'a','3',2]),[0,2]);
+// console.log(filter_list([1,2,3,4,'5']),[1,2,3,4]);
+// 
